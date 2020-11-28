ca4960a6275c57e4b599a3ee3d091f11fa9a81d8
Kacper Smal <kacpersmal@gmail.com>
2020-11-28 18:04:55 +0100

    UserPostsService planned

---
+++ GeoImagerApi/Services/Interfaces/IUserPostService.cs
@@ -0,0 +1,24 @@
+﻿using GeoImagerApi.Data.Models;
+using GeoImagerApi.DataTransferObjects.Request;
+using GeoImagerApi.DataTransferObjects.Response;
+using GeoImagerApi.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace GeoImagerApi.Services.Interfaces
+{
+    public interface IUserPostService
+    {
+        Task<UserPostsPaginatedResponse> GetUserPaginatedPosts(GetAllUserPostsPaginatedRequest req);        
+        Task<CreatePostResponse> CreatePost(CreatePostRequest req);
+        Task<CreatePostResponse> EditPost(EditPostRequest req);
+        Task<DeletePostResponse> DeletePost(DeletePostRequest req);
+        Task<GetFeedPostsPaginatedResponse> GetUserPaginatedFeed(GetFeedPostsPaginatedRequest req);
+        Task<GetPaginatedLocationPostsResponse> GetPaginatedPostsByLocation(GetPaginatedLocationPostsRequest req);
+        Task<CommentPostResponse> CommentPost(CommentUserPostRequest req);
+        Task<DeleteCommentPostResponse> DeleteComment(DeleteUserPostCommentRequest req);
+    }
+}
